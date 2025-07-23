79e5067aab9b9bbbaf3494b68ae8d77147a91626
dan-tovytch <danielmelentovytch@gmail.com>
2025-07-23 19:00:31 -0300

    (feat): ranking store para comunicação com a api

---
+++ interface/src/store/modules/ranking_store.js
@@ -0,0 +1,27 @@
+import ranking from "@/services/modules/ranking";
+import { defineStore } from "pinia";
+
+export const useRankingStore = defineStore("ranking", {
+  state: () => ({
+    data_ranking: [],
+    loading: true,
+  }),
+
+  actions: {
+    async FETCH_RANKING() {
+      try {
+        const response = await ranking.get_ranking();
+        if (response.data && response.data.data && Array.isArray(response.data.data.data)) {
+        this.data_ranking = response.data.data.data
+        } else {
+        console.error('Resposta não tem o array esperado:', response.data)
+        this.data_ranking = []
+        }
+      } catch (error) {
+        console.error("Erro ao buscar o ranking");
+      } finally {
+        this.loading = false;
+      }
+    },
+  },
+});
