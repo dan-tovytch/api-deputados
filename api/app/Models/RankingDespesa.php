e130085b55cdeb99b313d4a0f264ba9a462eac4c
dan-tovytch <danielmelentovytch@gmail.com>
2025-07-23 23:08:09 -0300

    Model RankingDespesas

---
+++ api/app/Models/RankingDespesa.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RankingDespesa extends Model
+{
+    protected $table = 'vw_ranking_despesas';
+    public $timestamps = false;
+}
